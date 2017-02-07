d300c8152a337049937001ca769f250284ba3ddf
Justin Nesselrotte <jnesselr@uber.com>
2017-02-07 11:56:27 -0700

    Updating creep roles to be state based.

--- src/roles/builder.js
+++ src/roles/builder.js
@@ -12,54 +12,64 @@ module.exports = {
   key: 'basic_builder',
   /** @param {Spawn} spawn **/
   needed: function (spawn) {
-    return 3;
+    return 5;
   },
   body: [CARRY, WORK, MOVE, MOVE, MOVE],
 
   /** @param {Creep} creep **/
   run: function (creep) {
-    let target;
-    if (creep.memory.building) {
-      if (creep.memory.target === null || typeof creep.memory.target === "undefined") {
+    switch (creep.memory.state) {
+      case 'Init':
+        creep.memory.state = 'Need_Energy';
+        break;
+
+      case 'Need_Energy':
+        if (creep.carry.energy < creep.carryCapacity) {
+          resource_fetch.fetch_closest_dropped_energy(creep);
+        } else {
+          creep.memory.state = 'Find_Target';
+        }
+        break;
+
+      case 'Find_Target':
         let targets = creep.room.find(FIND_CONSTRUCTION_SITES);
 
         targets = targets.sort((a, b) => {
           // TODO this fails when a structure isn't in our dictionary
-          a_order = build_order.indexOf(a.structureType);
-          b_order = build_order.indexOf(b.structureType);
+          let a_order = build_order.indexOf(a.structureType);
+          let b_order = build_order.indexOf(b.structureType);
           return a_order - b_order;
         });
 
+        // console.log(JSON.stringify(targets));
+
         if (targets.length > 0) {
-          target = targets[0];
+          let target = targets[0];
           console.log(`${creep.name} targeting ${target.structureType} at ${JSON.stringify(target.pos)} to build against`);
 
           if (target) {
             creep.memory.target = target.id;
+            creep.memory.state = 'Building';
+
             creep.say(target.structureType);
           }
         }
-      }
+        break;
 
-      target = Game.getObjectById(creep.memory.target);
-      if (target) {
-        let result = creep.build(target);
-        if (result == ERR_NOT_IN_RANGE) {
-          creep.moveTo(target);
-        } else if (result == ERR_NOT_ENOUGH_RESOURCES) {
-          creep.memory.building = false;
-          creep.memory.target = null;
+      case 'Building':
+        let target = Game.getObjectById(creep.memory.target);
+
+        if (target) {
+          let result = creep.build(target);
+          if (result == ERR_NOT_IN_RANGE) {
+            creep.moveTo(target);
+          } else if (result == ERR_NOT_ENOUGH_RESOURCES) {
+            creep.memory.state = 'Need_Energy';
+          }
+        } else {
+          creep.memory.state = 'Find_Target';
         }
-      } else {
-        creep.memory.building = false;
-        creep.memory.target = null;
-      }
-    } else {
-      if (creep.carry.energy < creep.carryCapacity) {
-        resource_fetch.fetch_closest_dropped_energy(creep);
-      } else {
-        creep.memory.building = true
-      }
+        break;
     }
   }
 };
