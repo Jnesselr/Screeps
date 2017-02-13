ed709de3504deaf62e16e8c830de49161a8a985b
Justin Nesselrotte <jnesselr@uber.com>
2017-02-12 23:06:47 -0700

    Adding tower repairing of roads

--- src/roles/creeps/transfer.js
+++ src/roles/creeps/transfer.js
@@ -4,13 +4,27 @@ let container_filter = function (structure) {
   return (structure.structureType == STRUCTURE_SPAWN && structure.energy < structure.energyCapacity) ||
     (structure.structureType == STRUCTURE_EXTENSION && structure.energy < structure.energyCapacity) ||
     (structure.structureType == STRUCTURE_CONTAINER && structure.energy < structure.storeCapacity) ||
-    (structure.structureType == STRUCTURE_TOWER && structure.energy < structure.energyCapacity)
+    (structure.structureType == STRUCTURE_TOWER && structure.energy < structure.energyCapacity) ||
+    (structure.structureType == STRUCTURE_STORAGE && _.sum(structure.store) < structure.storeCapacity)
+};
+
+const fill_order = [
+  STRUCTURE_SPAWN,
+  STRUCTURE_TOWER,
+  STRUCTURE_STORAGE,
+  STRUCTURE_EXTENSION
+];
+
+let container_sort = function(a, b) {
+  let a_order = fill_order.indexOf(a.structureType);
+  let b_order = fill_order.indexOf(b.structureType);
+  return a_order - b_order;
 };
 
 module.exports = {
   key: 'basic_transfer',
   type: roleType.CREEP,
-  needed: 1,
+  needed: 2,
   body: [CARRY, CARRY, MOVE, MOVE],
 
   /** @param {Creep} creep **/
@@ -29,13 +43,20 @@ module.exports = {
         break;
 
       case 'Transfer':
-        let targets = creep.room.find(FIND_STRUCTURES).filter(container_filter);
+        let targets = creep.room.find(FIND_STRUCTURES)
+          .filter(container_filter)
+          .sort(container_sort);
 
         if (targets.length > 0) {
           let result = creep.transfer(targets[0], RESOURCE_ENERGY);
           if (result == ERR_NOT_IN_RANGE) {
             if (creep.fatigue == 0)
-              creep.moveTo(targets[0]);
+              creep.moveTo(targets[0], {
+                visualizePathStyle: {
+                  stroke: '#ff0314',
+                  strokeWidth: .15,
+                }
+              });
           } else if (result == ERR_NOT_ENOUGH_RESOURCES) {
             creep.memory.state = 'Need_Energy';
           }
--- src/roles/tower.js
+++ src/roles/tower.js
@@ -3,5 +3,26 @@ module.exports = {
   key: 'tower',
   type: roleType.TOWER,
   run: function (tower) {
+    let things_to_repair =
+      Object.keys(Memory.structures.roads)
+      .map(function(road_id) {
+        return Game.getObjectById(road_id)
+      })
+      .filter(function(road) {
+        if (road == null)
+          return false;
+
+        return road.hits < (road.hitsMax - 800);
+      })
+      .sort(function(a, b) {
+        let a_ratio = a.hits / a.hitsMax;
+        let b_ratio = b.hits / b.hitsMax;
+        return a_ratio - b_ratio;
+      });
+
+    if(things_to_repair) {
+      // console.log(`Repairing ${things_to_repair[0].pos}`);
+      tower.repair(things_to_repair[0]);
+    }
   }
 };
